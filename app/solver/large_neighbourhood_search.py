2381a1f7c89b55785274ae7e14179d2eaac5f49f
this0is0kuba <jakub.rakus06@gmail.com>
2024-11-10 13:18:09 +0100

    change project structure

--- app/solver/large_neighbourhood_search.py
+++ app/solver/large_neighbourhood_search.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from minizinc import Result, Solver, Model, Instance
 
 from models import InputStudentGroups
@@ -17,12 +19,14 @@ class LNS:
 
     def solve(self) -> Solution:
 
+        print(-1, ") " "score: ", self.current_best_score, ", best score:", self.current_best_score)
+
         number_of_all_classes = self.input_student_groups.number_classes
         number_of_active_classes = min(3, number_of_all_classes)
 
-        for i in range(5):
+        for i in range(20):
 
-            model: Model = Model(r"./app/solver/minizinc/solvers/student_groups_lns.mzn")
+            model: Model = Model(r"./app/solver/minizinc/solvers/student_groups_lns_old.mzn")
             instance: Instance = self._create_instance_student_groups(self.solver, model)
 
             frozen_classes = self._find_random_classes(number_of_active_classes)
@@ -31,14 +35,14 @@ class LNS:
             instance["frozen_classes"] = frozen_classes
             instance["current_best_groups"] = self.current_best_solution
 
-            result = instance.solve(processes=8)
+            result = instance.solve(processes=8, timeout=timedelta(seconds=10))
             solution, score = result["student_group"], result["groups_with_common_students"]
 
-            if score >= self.current_best_score:
+            if score <= self.current_best_score:
                 self.current_best_score = score
                 self.current_best_solution = solution
 
-            print(i, ". " "score: ", score)
+            print(i, ") " "score: ", score, ", best score:", self.current_best_score)
 
         return self.current_best_solution
 
@@ -53,7 +57,7 @@ class LNS:
 
     def _find_initial_groups(self) -> (Solution, int):
 
-        model: Model = Model(r"./app/solver/minizinc/solvers/student_groups.mzn")
+        model: Model = Model(r"./app/solver/minizinc/solvers/student_groups_old.mzn")
         instance: Instance = self._create_instance_student_groups(self.solver, model)
 
         initial_solution = instance.solve(processes=8)
